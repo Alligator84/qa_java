9cb1ac49019d4d08fb1fa7188daa66bed6cb7af6
Oleg Yudin <dev@alligator84.ru>
2022-12-14 10:55:04 +0600

    Add lion tests

--- src/test/java/LionExceptionTest.java
+++ src/test/java/LionExceptionTest.java
@@ -1,2 +1,20 @@
-public class CatExceptionTest {
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LionExceptionTest {
+
+    @Mock
+    Feline feline;
+
+    @Test
+    public void lionTest() {
+        Exception exception = Assertions.assertThrows(Exception.class, () -> new Lion("", feline));
+        Assertions.assertEquals("Используйте допустимые значения пола животного - самей или самка", exception.getMessage());
+    }
 }
